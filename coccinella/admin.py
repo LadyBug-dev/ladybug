c7c726e36442e08fa8af4cf3e0a4f1aa5ec7b35a
blazingsaint <cortez505amom@gmail.com>
2024-03-12 22:10:18 +0000

    Adição da pasta templates e atualização das configurações

--- coccinella/admin.py
+++ coccinella/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.db import models
+
+admin.site.register(models.StockItem)
+admin.site.register(models.SourceItem)
+admin.site.register(models.SalesChannel)
+admin.site.register(models.Sale)
+admin.site.register(models.StockSale)
 
 # Register your models here.
